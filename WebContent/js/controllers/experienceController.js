155afbe0ae863671cdbf8c29e379b9316a17fb76
Vianney Dubus <vianney.dubus@gmail.com>
2016-03-30 14:02:59 +0200

    Test $http service
    Get mock data other an outside service.

--- WebContent/js/controllers/experienceController.js
+++ WebContent/js/controllers/experienceController.js
@@ -1,45 +1,10 @@
 /**
  * 
  */
-app.controller("experienceController", [ "$scope", function($scope) {
-	$scope.experienceList = [ {
-		"enterpriseName" : "Enterprise 01",
-		"posteDescription" : "Work description",
-		"projectList" : [ {
-			"projectName" : "Project 01",
-			"projectDescription" : "Project 01 description"
-		}, {
-			"projectName" : "Project 02",
-			"projectDescription" : "Project 02 description"
-		}, {
-			"projectName" : "Project 03",
-			"projectDescription" : "Project 03 description"
-		} ]
-	}, {
-		"enterpriseName" : "Enterprise 02",
-		"posteDescription" : "Work description",
-		"projectList" : [ {
-			"projectName" : "Project 01",
-			"projectDescription" : "Project 01 description"
-		}, {
-			"projectName" : "Project 02",
-			"projectDescription" : "Project 02 description"
-		}, {
-			"projectName" : "Project 03",
-			"projectDescription" : "Project 03 description"
-		} ]
-	}, {
-		"enterpriseName" : "Enterprise 03",
-		"posteDescription" : "Work description",
-		"projectList" : [ {
-			"projectName" : "Project 01",
-			"projectDescription" : "Project 01 description"
-		}, {
-			"projectName" : "Project 02",
-			"projectDescription" : "Project 02 description"
-		}, {
-			"projectName" : "Project 03",
-			"projectDescription" : "Project 03 description"
-		} ]
-	} ];
+app.controller("experienceController", [ "$scope", "$http", function($scope, $http) {
+	$http.get("http://www.mocky.io/v2/56fbbf211000003522687974").then(function(response) {
+		$scope.experienceList = response.data;
+	}, function(response) {
+		alert('error');
+	});
 } ]);
