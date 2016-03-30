be1252ad1d305db20f4ee7f37837a7e674267c49
Vianney Dubus <vianney.dubus@gmail.com>
2016-03-30 19:04:37 +0200

    Manage hobbies with a JSON mock
    Add a data mock to fill the hobbies in the hobbyController.

--- WebContent/js/controllers/hobbyController.js
+++ WebContent/js/controllers/hobbyController.js
@@ -1,6 +1,10 @@
 /**
  * 
  */
-app.controller("hobbyController", ["$scope", function($scope) {
-	$scope.hobbyList = [ "hobby 01", "hobby 02", "hobby 03" ];
+app.controller("hobbyController", ["$scope", "$http", function($scope, $http) {
+	$http.get("mocks/hobbyDataMock.json").then(function(response) {
+		$scope.hobbyList = response.data;
+	}, function(response) {
+		alert('error hobby');
+	});
 }]);
