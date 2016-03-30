007af8064ba5c726f7aec0784e4f413937d91d9e
Vianney Dubus <vianney.dubus@gmail.com>
2016-03-30 18:56:34 +0200

    Remove links to external resources
    Remove links to google CDN libraries and to JSON mockio website.

--- WebContent/js/controllers/experienceController.js
+++ WebContent/js/controllers/experienceController.js
@@ -2,7 +2,7 @@
  * 
  */
 app.controller("experienceController", [ "$scope", "$http", function($scope, $http) {
-	$http.get("http://www.mocky.io/v2/56fbbf211000003522687974").then(function(response) {
+	$http.get("mocks/experienceDataMock.json").then(function(response) {
 		$scope.experienceList = response.data;
 	}, function(response) {
 		alert('error');
