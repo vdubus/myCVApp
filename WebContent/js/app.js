71163560154617e42e9bcd3708ffe257b3cb213c
Vianney Dubus <vianney.dubus@gmail.com>
2016-04-14 22:09:30 +0200

    Add route management
    Add a route management to display experiences.

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,4 +1,18 @@
 /**
- * 
+ *
  */
-var app = angular.module("app", []);
+var app = angular.module("app", ["ngRoute"]);
+app.controller("editExpController", ["$scope", function ($scope) {
+
+}]);
+app.config(function ($routeProvider) {
+	$routeProvider.when('/', {
+		controller:'experienceController as experienceController',
+		templateUrl:'templates/experience/list.html'
+	}).when('/experience/new', {
+		templateUrl: 'templates/experience/edit.html',
+		controller: 'editExpController'
+	}).otherwise({
+		redirectTo: "/"
+	});
+});
