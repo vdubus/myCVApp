b584aa578454791eb0444a0fc89596264827f37e
Vianney Dubus <vianney.dubus@gmail.com>
2016-04-16 10:28:50 +0200

    Use a view to display the App
    Move the whole application as a template to be able to change the edit
    view for each parts.

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -7,8 +7,8 @@ app.controller("editExpController", ["$scope", function ($scope) {
 }]);
 app.config(function ($routeProvider) {
 	$routeProvider.when('/', {
-		controller:'experienceController as experienceController',
-		templateUrl:'templates/experience/list.html'
+		controller:'displayController',
+		templateUrl:'templates/display.html'
 	}).when('/experience/new', {
 		templateUrl: 'templates/experience/edit.html',
 		controller: 'editExpController'
