4d0f3be6fd08cd258036861dc326702dea9cfcb9
Vianney Dubus <vianney.dubus@gmail.com>
2016-04-17 23:05:23 +0200

    First edit step
    First step to manage data edition.

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -3,7 +3,13 @@
  */
 var app = angular.module("app", ["ngRoute"]);
 app.controller("editExpController", ["$scope", function ($scope) {
-
+	// localStorage
+	var editCtrl = this;
+	editCtrl.values = [];
+	editCtrl.add = function(){
+		editCtrl.values.push(editCtrl.value);
+		editCtrl.value = '';
+	}
 }]);
 app.config(function ($routeProvider) {
 	$routeProvider.when('/', {
@@ -11,7 +17,7 @@ app.config(function ($routeProvider) {
 		templateUrl:'templates/main.html'
 	}).when('/experience/new', {
 		templateUrl: 'templates/experience/edit.html',
-		controller: 'editExpController'
+		controller: 'editExpController as editCtrl'
 	}).otherwise({
 		redirectTo: "/"
 	});
